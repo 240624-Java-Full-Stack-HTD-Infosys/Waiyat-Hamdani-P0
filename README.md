c7944ce19bd82a0ac13bbd975d46f3311aefaa12
waiyat.hamdani <waiyat.hamdani@fdmgroup.com>
2024-06-25 16:03:52 -0400

    just creating draft for User for project 0

--- README.md
+++
@@ -1,3 +0,0 @@
-# Waiyat-Hamdani-P0
-
-04/24/2024 ->  planning to git push using thiscommand 
---
+++ untitled/src/BankingSystem/User.java
@@ -0,0 +1,22 @@
+package BankingSystem;
+import java.util.HashMap;
+
+public class User {
+    enum TypeUser{
+        GUESS,
+        CurrentUser
+    }
+
+    private String Firstname;
+    private String Lastname;
+    private int userId;
+    private String Email;
+    private double Balance;
+
+    public User(String Firstname, String Lastname){
+
+    }
+
+
+
+}
