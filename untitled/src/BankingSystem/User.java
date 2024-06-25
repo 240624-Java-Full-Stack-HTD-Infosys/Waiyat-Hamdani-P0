d455509cccfd9294af75513a46684b143b60b51b
waiyat.hamdani <waiyat.hamdani@fdmgroup.com>
2024-06-25 16:17:20 -0400

    setting up first method for user

--- untitled/src/BankingSystem/User.java
+++ untitled/src/BankingSystem/User.java
@@ -3,16 +3,23 @@ package BankingSystem;
 public class User {
     enum TypeUser{
         GUESS,
-        CLIENT
+        CLIENT,
+        ADMIN
     }
 
     private String Firstname;
     private String Lastname;
+    private String Address;
     private int userId;
     private String Email;
     private double Balance;
 
-    public User(String Firstname, String Lastname){
+    public User(String firstname, String lastname , String email, String add){
+        super();
+        Firstname = firstname;
+        Lastname = lastname;
+        Email =  email;
+        Address = add;
 
     }
 
